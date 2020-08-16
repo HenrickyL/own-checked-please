b4dc3f004972955227d962dfc7c9a162e11d7374
HenrickyL <henrickyL1@gmail.com>
2020-08-16 02:13:06 -0300

    frontEnd

--- public/scripts/buys.js
+++ public/scripts/buys.js
@@ -1,10 +1,50 @@
 
 import header from "./header.js"
+import {leftpad} from "./timestamp.js"
 
 window.addEventListener("load",()=>{
     header.loadButons()
+       let popup = document.querySelector(".add-popup")
+       let addBox = document.querySelector(".add-box")
+       let d = document.getElementById("buy-description")
+       let v = document.getElementById("buy-value")
+       let add = document.querySelector(".bt-add")
+       let submit = document.getElementById("add-submit")
+       let elemen = [d,v]
+       /////////////////////
+       let dateNow = document.querySelector("#date-now")
     header.activateButtons()
-    
+        elemen.forEach(elem=>{
+            elem.addEventListener("blur",event=>{
+                // if(event.key === "Enter"){
+                    
+                    let valor = v.value%1==0? (v.value===''?'0':v.value)+",00" : v.value;
+                    let note = `\n------------------------\nR$ ${valor}`
+
+                    if(elem.textContent.search("------------------------")===-1){
+                        d.textContent += note
+                    }else if(d.textContent.lastIndexOf("\n")>0){
+                        let x =d.textContent
+                        d.textContent = x.substring(0, x.lastIndexOf("\n"));
+                        x = d.textContent
+                        d.textContent = x.substring(0, x.lastIndexOf("\n"));
+                        x = d.textContent
+                        d.textContent = x.substring(0, x.lastIndexOf("\n"));
+                        d.textContent += note
+                    }
+            })
+        })
+        add.addEventListener("click",()=>{
+            let d = new Date()
+            dateNow.textContent = `${leftpad(d.getDate())}/${leftpad(d.getMonth()+1)} - ${leftpad(d.getHours())}:${leftpad(d.getMinutes())}`
+            popup.classList.remove("hide")
+            addBox.classList.remove("hide")
+        })
+        
+        popup.addEventListener("click",()=>{
+            addBox.classList.add("hide")
+            popup.classList.add("hide")
+        })   
     
     console.log("<Page Loaded!>")
 
--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,23 +1,76 @@
 let btReturn = null
-let btOptions = null
 
+let btOp = null
+let menuBox = null
+
+let popup = null
+let addBox = null
+let d = null
+let v = null
+let add = null
+let submit = null
+let elemen = [d,v]
+
+let searchBar = null
+let searchInput = null
+let searchButton = null
 
 export function loadButons(){
     btReturn = document.querySelector(".bt-return")
-    btOptions = document.querySelector(".bt-options")
+    btOp = document.querySelector(".bt-options")
+    menuBox = document.querySelector(".menu-op")
+    searchBar = document.querySelector('.search-bar')
+    searchInput = document.querySelector('.search-bar input')
+    searchButton = document.querySelector('.search-bar button')
+    ////////////////////////////
+    
+
     console.log("Loaded buttons!")
 }
 export function activateButtons(){
     btReturn.addEventListener("click",()=>{
         window.location = "/"
     })
-    btOptions.addEventListener("click",()=>{
-        // window.location = "/search"
-        console.log("Options Clicked")
+    
+    btOp.addEventListener('focus',()=>{
+        menuBox.classList.add("focused")
     })
+    btOp.addEventListener("blur",()=>{
+        menuBox.classList.remove("focused")
+    })
+//////////////////////////////
+    searchButton.addEventListener("keyup",()=>{
+        if(event.key ==='Enter'){
+            //busca
+        }
+    })
+    searchButton.addEventListener("click",()=>{
+        searchBar.classList.add('focused')
+        if(searchButton.value.length > 0 ){
+            //buscar
+        }
+        setTimeout(()=>{
+
+            searchInput.focus()
+        },200)
+        
+    })
+    searchInput.addEventListener("focusout",()=>{
+        setTimeout(()=>{
+            searchBar.classList.remove('focused')
+        },500)
+    })
+
+
     console.log("activated buttons")
 }
 
 const header = {loadButons,activateButtons}
 
 export default header
+
+
+
+
+            
+            
